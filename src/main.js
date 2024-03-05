956061ff02c15ffa7ba8c7c0edcfba076f101234
rookiebyte <tvpozytyw@gmail.com>
2024-03-05 17:52:26 +0100

    Total refactor like client wanted

--- src/main.js
+++ src/main.js
@@ -1,14 +1,6 @@
 const $ = document.querySelector.bind(document);
 const $$ = document.querySelectorAll.bind(document);
 
-// document.querySelector('html').classList.add('dark')
-
-window.addEventListener("load", (event) => {
-    for(const elem of $$(".page")) {
-        elem.style = "height: " + window.innerHeight + "px;";
-    }
-});
-
 function moveTo(name) {
     window.location = name;
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,4 +2,11 @@
 module.exports = {
   content: ['./src/**/*.{html,js}'],
   darkMode: 'class',
+  theme: {
+    colors: {
+      second: '#8D8D8D',
+      primary: '#19976B',
+      black: '#000000'
+    },
+  },
 }
